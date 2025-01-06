88840c4262fe7e07e29b1e90b9b907b476e84c07
PhilBou2024 <philippe.bourrat@orange.com>
2025-01-06 09:53:54 -0500

    Conditional - Pratice task - to be continued

---
+++ jsControlFlow/dietary_access_control.js
@@ -0,0 +1,14 @@
+let userRole;
+let accessLevel;
+let isLoggedIn;
+let userMessage;
+let userType;
+let userCategory; 
+let isAuthenticated; 
+let authenticationStatus;
+
+if (userRole === "") {
+
+}
+
+authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated"
